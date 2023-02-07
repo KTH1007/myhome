81d81abb160abc738bfbbb6e2700a9b4f323ba90
KTH1007 <rlaxogjs2684@gmail.com>
2023-02-07 20:30:57 +0900

    글 작성과 조회, 페이징 기능 추가

---
+++ src/main/java/com/project/myhome/model/Board.java
@@ -0,0 +1,23 @@
+package com.project.myhome.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+
+@Entity
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotNull
+    @Size(min=2,max=50, message = "제목은 2자 이상 5자 이하입니다.")
+    private String title;
+    private String content;
+}
