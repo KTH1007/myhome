116272fa069493a4c7c7fccb488311ac4901416e
KTH1007 <rlaxogjs2684@gmail.com>
2023-02-18 00:17:57 +0900

    수정, 삭제 권한 체크

--- src/main/java/com/project/myhome/controller/BoardApiController.java
+++ src/main/java/com/project/myhome/controller/BoardApiController.java
@@ -3,6 +3,7 @@ package com.project.myhome.controller;
 import com.project.myhome.model.Board;
 import com.project.myhome.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
 
@@ -55,6 +56,7 @@ class BoardApiController {
                 });
     }
 
+    @Secured("ROLE_ADMIN")
     @DeleteMapping("/boards/{id}")
     void deleteBoard(@PathVariable Long id) {
         repository.deleteById(id);
