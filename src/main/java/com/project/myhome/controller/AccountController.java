eee0c5b2abbbe870822a629a0f89519e1eb26e85
KTH1007 <rlaxogjs2684@gmail.com>
2023-03-30 23:37:18 +0900

    feat: 삭제 권한 체크 fix: 회원가입 오류 수정

--- src/main/java/com/project/myhome/controller/AccountController.java
+++ src/main/java/com/project/myhome/controller/AccountController.java
@@ -26,8 +26,9 @@ public class AccountController {
         return "account/register";
     }
 
+
     @PostMapping("/register")
-    public String register(User user, @RequestParam String username, @RequestParam String password1, @RequestParam String password2, Model model){
+    public String register(@RequestParam String username, @RequestParam String password1, @RequestParam String password2, Model model){
         if (userService.checkUserName(username)) {
             model.addAttribute("usernameError", "이미 사용 중인 아이디입니다.");
             return "account/register";
@@ -36,7 +37,11 @@ public class AccountController {
             model.addAttribute("passwordError", "비밀번호가 일치하지 않습니다.");
             return "account/register";
         }
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password1);
         userService.save(user);
         return "redirect:/";
     }
+
 }
--- src/main/java/com/project/myhome/controller/BoardApiController.java
+++ src/main/java/com/project/myhome/controller/BoardApiController.java
@@ -3,7 +3,7 @@ package com.project.myhome.controller;
 import com.project.myhome.model.Board;
 import com.project.myhome.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
 
@@ -14,23 +14,23 @@ import java.util.List;
 class BoardApiController {
 
     @Autowired
-    private BoardRepository repository;
+    private BoardRepository boardRepository;
+
 
     @GetMapping("/boards")
     List<Board> all(@RequestParam(required = false, defaultValue = "") String title,
-        @RequestParam(required = false, defaultValue = "") String content) {
-        if(StringUtils.isEmpty(title) && StringUtils.isEmpty(content)){
-            return repository.findAll();
-        }
-        else{
-            return repository.findByTitleOrContent(title, content);
+                    @RequestParam(required = false, defaultValue = "") String content) {
+        if (StringUtils.isEmpty(title) && StringUtils.isEmpty(content)) {
+            return boardRepository.findAll();
+        } else {
+            return boardRepository.findByTitleOrContent(title, content);
         }
     }
     // end::get-aggregate-root[]
 
     @PostMapping("/boards")
     Board newBoard(@RequestBody Board newBoard) {
-        return repository.save(newBoard);
+        return boardRepository.save(newBoard);
     }
 
     // Single item
@@ -38,27 +38,30 @@ class BoardApiController {
     @GetMapping("/boards/{id}")
     Board one(@PathVariable Long id) {
 
-        return repository.findById(id).orElse(null);
+        return boardRepository.findById(id).orElse(null);
     }
 
     @PutMapping("/boards/{id}")
     Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
 
-        return repository.findById(id)
+        return boardRepository.findById(id)
                 .map(board -> {
                     board.setTitle(newBoard.getTitle());
                     board.setContent(newBoard.getContent());
-                    return repository.save(board);
+                    return boardRepository.save(board);
                 })
                 .orElseGet(() -> {
                     newBoard.setId(id);
-                    return repository.save(newBoard);
+                    return boardRepository.save(newBoard);
                 });
     }
 
-//    @Secured("ROLE_ADMIN")
+    //@Secured("ROLE_ADMIN")
+
+    @PreAuthorize("hasRole('ADMIN') or #board.user.username == authentication.name")
     @DeleteMapping("/boards/{id}")
     void deleteBoard(@PathVariable Long id) {
-        repository.deleteById(id);
+        boardRepository.deleteById(id);
     }
+
 }
