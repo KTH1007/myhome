fe914be21878cb25896a04244a4e4ecc681d39b8
KTH1007 <rlaxogjs2684@gmail.com>
2023-04-03 16:20:40 +0900

    Feat: 5페이지씩 표시되도록 페이징 처리 수정

--- src/main/java/com/project/myhome/controller/BoardController.java
+++ src/main/java/com/project/myhome/controller/BoardController.java
@@ -35,8 +35,10 @@ public class BoardController {
     public String list(Model model, @PageableDefault(size = 2) Pageable pageable, @RequestParam(required = false, defaultValue = "") String searchText){
         //Page<Board> boards =  boardRepository.findAll(pageable);
         Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText,searchText,pageable);
-        int startPage = Math.max(1,boards.getPageable().getPageNumber() - 4);
-        int endPage = boards.getTotalPages();
+        int block = 5;
+        int currentBlock = (boards.getPageable().getPageNumber() / block) * block;
+        int startPage = currentBlock + 1;
+        int endPage = Math.min(boards.getTotalPages(), currentBlock + block);
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
