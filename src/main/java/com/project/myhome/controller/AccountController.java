55283883f85ec89ffed03f6bd65ed67098e79071
KTH1007 <rlaxogjs2684@gmail.com>
2023-05-24 15:18:17 +0900

    Feat: 회원가입 요구조건 수정

--- src/main/java/com/project/myhome/controller/AccountController.java
+++ src/main/java/com/project/myhome/controller/AccountController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Controller
 @RequestMapping("/account")
 public class AccountController {
@@ -37,6 +40,21 @@ public class AccountController {
         }
         if(!password1.equals(password2)){
             model.addAttribute("passwordError", "비밀번호가 일치하지 않습니다.");
+            return "account/register";
+        }
+        if(username.length() < 6){
+            model.addAttribute("usernameError", "id는 6~20자리 이상이어야 됩니다.");
+            return "account/register";
+        }
+        if(password1.length() < 8){
+            model.addAttribute("passwordError", "비밀번호는 8자리 이상이어야 됩니다.");
+            return "account/register";
+        }
+        String pattern = "^(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*+=]).+$";
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(password1);
+        if(!matcher.matches()){
+            model.addAttribute("passwordError", "비밀번호는 영어, 숫자, 특수문자(!, @, #, $, %, ^, &, *, +,=)가 포함되어야 합니다.");
             return "account/register";
         }
         User user = new User();
